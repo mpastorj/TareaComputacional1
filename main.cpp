62089494f28827a053c5844dfd15debb864d827c
Milii2 <Milii2@Milii-PC>
2013-03-26 21:50:51 -0400

    Inicialización de Matriz

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Milii2
+ *
+ * Created on 26 de marzo de 2013, 09:47 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+        char M[8][8];
+        int i=0;
+        int j=0;
+        for(i=0;i<8;i++){
+                      for(j=0;j<8;j++){
+                                       M[i][j]='O';
+                                       }
+                      }
+        M[2][2]='O';
+        M[2][5]='O';
+        M[5][2]='O';
+        M[5][5]='O';
+        
+        return 0;
+}
+
